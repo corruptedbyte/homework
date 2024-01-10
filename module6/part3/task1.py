59e74c5d7aab87b7abf9902e9dabe12feec5274a
corruptedbyte <152075924+corruptedbyte@users.noreply.github.com>
2024-01-10 20:25:06 +0200

    Add files via upload

--- module6/part3/task1.py
+++ module6/part3/task1.py
@@ -1,2 +1,8 @@
-def largestDivisor(num1, num2):
-    pass
+def factorial(num):
+    if num == 0 or num == 1:
+        return 1
+    else:
+        return num*factorial(num-1)
+
+def largestDivisor(*numbers):
+    print(list(factorial(num) for num in numbers))
